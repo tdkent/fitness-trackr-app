cd606cc522c89b719c894ee7aff8478cc0f7c5f6
Tim Kent <timdekent@gmail.com>
2022-07-10 16:05:48 -0700

    updating css

--- src/components/DatabaseMessage.js
+++ src/components/DatabaseMessage.js
@@ -1,24 +1,41 @@
 import { useNavigate } from "react-router-dom";
 import Modal from "./Modal";
 
-const DatabaseMessage = ({dbMessage, setDbMessage, setUseModal, setCreateRoutine}) => {
+const DatabaseMessage = ({
+  dbMessage,
+  setDbMessage,
+  setUseModal,
+  setCreateRoutine,
+}) => {
   let navigate = useNavigate();
   const clickHandler = () => {
     setUseModal(false);
-    (dbMessage === "you're logged in!" || dbMessage === "New user created!") && navigate("/");
+    (dbMessage === "you're logged in!" || dbMessage === "New user created!") &&
+      navigate("/");
     setDbMessage("");
-  }
+  };
   return (
-    <Modal setUseModal={setUseModal} dbMessage={dbMessage} setCreateRoutine={setCreateRoutine} >
+    <Modal
+      setUseModal={setUseModal}
+      dbMessage={dbMessage}
+      setCreateRoutine={setCreateRoutine}
+    >
       <header>
-      {(dbMessage !== "you're logged in!" || dbMessage !== "New user created!") && <h3>There was an error!</h3>}
+        {dbMessage === "you're logged in!" ||
+        dbMessage === "New user created!" ? (
+          <h3>Success!</h3>
+        ) : (
+          <h3>There was an error!</h3>
+        )}
         <p>{dbMessage}</p>
       </header>
       <div>
-        <button onClick={clickHandler} className="primary">Okay</button>
+        <button onClick={clickHandler} className="primary">
+          Okay
+        </button>
       </div>
     </Modal>
-  )
-}
+  );
+};
 
 export default DatabaseMessage;
